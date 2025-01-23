c6bdf85966b0b57d7ea7b5b1a56cd6c7e4b50f6c
Rodrigo de Sousa <rs-rodrigosousa-1998@hotmail.com>
2025-01-23 10:29:08 +0000

    feat: code refactoring
    Signed-off-by: Rodrigo de Sousa <rs-rodrigosousa-1998@hotmail.com>

--- code_1.py
+++ code_1.py
@@ -3,108 +3,156 @@
 #TODO: Create script to initialize after turning RPi on and to turn the RPi off after pressing for more than 4 seconds
 #TODO: Implement buzer to play for the time the button is being pushed
 #TODO: Create menus for the OLED SS1306
-# import os
 
-# # Set the revision to the new-style revision code for the board (800013 for revision 000F) to work with rpi-lgpio module
-# os.environ['RPI_LGPIO_REVISION'] = '800013'
-
+# Python Standard Libraries 
 import time
 import math
 from enum import Enum
 
+# Modules to interact with the hardware
 import smbus2
 import RPi.GPIO as GPIO
 
+# Modules to interact with the connected components
 from LSM6DS3 import LSM6DS3
 from SSD1306 import SSD1306
 
-# Define I2C bus
+# GPIO Configuration
+SHORT_PRESS_THRESHOLD = 1.0  # seconds - short press duration
+PUSH_BUTTON_PIN = 17 # BCM GPIO 17 = Pin 11 in P1 Header
+GPIO.setmode(GPIO.BCM) # Set pin-numbering scheme to BCM Pinout 
+GPIO.setup(PUSH_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set GPIO pins to I/O and activate internal pull-up resistances
+
+# I2C Bus Definition
 bus = smbus2.SMBus(1)
 
-# Pin definitions
-pushButton = 17 # BCM GPIO 17 = Pin 11 in P1 Header
+# IMU Initialization
+print("Initializing LSM6DS3...")
+lsm6ds3 = LSM6DS3(bus)
 
-# Set pin-numbering scheme to BCM Pinout 
-GPIO.setmode(GPIO.BCM)
+# Display Initialzartion
+print("Initializing SSD1306...")
+oled = SSD1306()
 
-# Set GPIO pins to I/O and activate internal pull-up resistances
-GPIO.setup(pushButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# Machine States Definition
+class option(Enum):
+    MAIN_MENU      = 1
+    MEASURING_MODE = 2
+    DIAG_MODE      = 3
+    SETTINGS_MENU  = 4
+    CREDITS        = 5
+
+# Measument Modes Definition
+class measure(Enum):
+    ANGLE    = 1
+    DISTANCE = 2
+    START    = 3
+    STOP     = 4
+    IDLE     = 5
+
+# Global Variables Definition
+current_menu            = option.MAIN_MENU # State variable for mode selection
+button_pressed          = False            # Flag to prevent multiple detections
+button_press_start_time = None             # Variable to track press time
+measurement             = measure.IDLE     # Flag to dectect begining of measurement
+measure_mode            = measure.DISTANCE # Alternating variable to change types of measurement
+
+acc_y             = 0    # Variable to store accelaration on the Y axis
+vel_y             = 0    # Variable to store velocity on the Y axis
+dist_y            = 0    # Variable to store distance on the Y axis
+dps_y             = 0    # Variable to store angular speed on the Y axis
+deg_y             = 0    # Variable to store rotation on the Y axis
+sampling_interval = 0.1  # Sampling interval because IMU Output rate at 12.5 hz or 0.08s
+
+distances = []  # List of distances measured 
+rotations = [0] # List of rotations measured
+
+x_coords = []         # List to store X coordinates
+y_coords = []         # List to store Y coordinates
+resulting_vector = [] # List to store X,Y coordinates of resulting vector
 
 # Callback function for button press/release
 def button_press_callback(channel):
     global button_press_start_time, button_pressed, current_menu
 
     if GPIO.input(channel) == GPIO.HIGH:  # Button pressed (rising edge)
-        if not button_pressed:  # Ensure not to detect the same press multiple times
-            button_pressed = True
+        #if not button_pressed:  # Ensure not to detect the same press multiple times
             button_press_start_time = time.time()  # Record the start time of the press
+            #button_pressed = True
             #print("Button pressed!")
-            return
     
     if GPIO.input(channel) == GPIO.LOW:  # Button released (falling edge)
-        if button_pressed:  # Only handle release if the button was previously pressed
+       # if button_pressed:  # Only handle release if the button was previously pressed
             press_duration = time.time() - button_press_start_time  # Calculate how long the button was pressed
            
             if press_duration < SHORT_PRESS_THRESHOLD:
                 #print("Short press detected!")
-                # Handle short press
-
-                if current_menu is option.MEASURING_MODE: 
-                    global measurement
-                    if measurement is measure.STOP or measurement is measure.IDLE:
-                        print("Measurement started!")
-                        measurement = measure.START
-                    elif measurement is measure.START:
-                        print("Measurement stoped!")
-                        measurement = measure.STOP
-                else:
-                    current_menu = option(current_menu.value + 1)
-                    if current_menu == option.CREDITS:
-                        current_menu = option.MAIN_MENU
-                #TODO: Put here a otpion for being in the main menu, where each button press increments a selector that
-                #has a maximum value of the number of menu options
-   
+                handle_short_press()
             else:
                 #print("Long press detected!")
-                # Handle long press
+                handle_long_press()
 
-                if current_menu is option.MEASURING_MODE:
-                    print("Back to main menu after measuring!")
-                    current_menu = option.MAIN_MENU
-                    
-                    # Display results
-                    calculate_perimiter()
-
-                    # Reset Measuring Variables
-                    reset_measurement_variables()
-
-                elif current_menu is not option.MAIN_MENU:
-                    current_menu = option.MAIN_MENU
-                #TODO: Put here a option that in case in the main menu, it changes the current menu value to the selector value
-
-            button_pressed = False  # Reset the press state
-            return
+           # button_pressed = False  # Reset the press state
         
+def handle_short_press():
+    
+    global current_menu, measurement
+    
+    if current_menu is option.MEASURING_MODE: 
+        if measurement is measure.STOP or measurement is measure.IDLE:
+            print("Measurement started!")
+            measurement = measure.START
+        elif measurement is measure.START:
+            print("Measurement stoped!")
+            measurement = measure.STOP
+    else:
+        current_menu = option(current_menu.value + 1)
+        if current_menu == option.CREDITS:
+            current_menu = option.MAIN_MENU
+    #TODO: Put here a otpion for being in the main menu, where each button press increments a selector that
+    #has a maximum value of the number of menu options
+
+def handle_long_press():
+    
+    global current_menu
+    
+    if current_menu is option.MEASURING_MODE:
+        print("Back to main menu after measuring!")
+        current_menu = option.MAIN_MENU
+                    
+        # Display results
+        calculate_perimiter()
+
+        # Reset Measuring Variables
+        reset_measurement_variables()
+
+    elif current_menu is not option.MAIN_MENU:
+        current_menu = option.MAIN_MENU
+        #TODO: Put here a option that in case in the main menu, it changes the current menu value to the selector value
+
+
 def reset_measurement_variables():
+    
     global acc_y, vel_y, dist_y, dps_y, deg_y, sampling_interval, measure_mode
+    
     acc_y = vel_y = dist_y = dps_y = deg_y = 0
     sampling_interval = 0.1
     measure_mode = measure.DISTANCE
 
 def calculate_perimiter():
-    global rotations
+    
+    global distances, rotations
+    
     for rotation in rotations:
         print(rotation)
 
-    global distances
     for distance in distances:
         print(distance)
 
     # Convert to cartesian coordinates
     if len(rotations) == len (distances):
-        global x_coords
-        global y_coords
-        global resulting_vector
+
+        global x_coords, y_coords, resulting_vector
         
         # Iterate over the polar coordinates
         for r, angle_deg in zip(distances, rotations):
@@ -131,62 +179,14 @@ def calculate_perimiter():
     y_coords = []
     resulting_vector = []
 
-
-# Machine States Definition
-class option(Enum):
-    MAIN_MENU = 1
-    MEASURING_MODE = 2
-    DIAG_MODE = 3
-    SETTINGS_MENU = 4
-    CREDITS = 5
-
-# Measument Modes Definition
-class measure(Enum):
-    ANGLE = 1
-    DISTANCE = 2
-    START = 3
-    STOP = 4
-    IDLE = 5
-
-# Short and Long Press Thresholds Definition
-SHORT_PRESS_THRESHOLD = 1.0  # seconds (short press duration)
-
-# Global Variables Definition
-current_menu = option.MAIN_MENU # State variable for mode selection
-button_press_start_time = None # Variable to track press time
-button_pressed = False # Flag to prevent multiple detections
-
-measurement = measure.IDLE # Flag to dectect begining of measurement
-measure_mode = measure.DISTANCE # Alternating variable to change types of measurement
-acc_y = 0
-vel_y = 0
-dist_y = 0
-dps_y = 0
-deg_y = 0
-sampling_interval = 0.1  # Because IMU Output rate at 12.5 hz or 0.08s
-
-# List of distances and rotation measured 
-distances = []
-rotations = [0]
-
-# Lists to store cartesian coordinates
-x_coords = []
-y_coords = []
-resulting_vector = []
 #!############################## --- EXECUTIVE CYCLE --- ###############################
 
 if __name__ == "__main__":
     
-    print("Initializing SSD1306...")
-    oled = SSD1306()
-    
-    print("Initializing LSM6DS3...")
-    lsm6ds3 = LSM6DS3(bus)
-
     acc_scaling_factor = 0.000061 # Sensitivity/Resolution for +-2g scale
     dps_scaling_factor = 0.0035  # Sensitivity/Resolution for +-1000dps scale
 
-    GPIO.add_event_detect(pushButton, GPIO.BOTH, callback=button_press_callback, bouncetime=150)
+    GPIO.add_event_detect(PUSH_BUTTON_PIN, GPIO.BOTH, callback=button_press_callback, bouncetime=150)
 
     try:
         while True:
@@ -195,6 +195,7 @@ if __name__ == "__main__":
                 print("Press to start measurement...")
 
                 while current_menu == option.MAIN_MENU:
+                    #print("here")
                     time.sleep(0.1)
 
             elif current_menu == option.MEASURING_MODE:
