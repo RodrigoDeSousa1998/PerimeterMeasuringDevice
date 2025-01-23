10bee2e9ddba6a7b16927886328c840b2e21bdf3
Rodrigo de Sousa <rs-rodrigosousa-1998@hotmail.com>
2025-01-23 09:01:44 +0000

    feat: chaged match case to elif statement because of python 3.9
    Signed-off-by: Rodrigo de Sousa <rs-rodrigosousa-1998@hotmail.com>

--- code_1.py
+++ code_1.py
@@ -3,10 +3,10 @@
 #TODO: Create script to initialize after turning RPi on and to turn the RPi off after pressing for more than 4 seconds
 #TODO: Implement buzer to play for the time the button is being pushed
 #TODO: Create menus for the OLED SS1306
-import os
+# import os
 
-# Set the revision to the new-style revision code for the board (800013 for revision 000F) to work with rpi-lgpio module
-os.environ['RPI_LGPIO_REVISION'] = '800013'
+# # Set the revision to the new-style revision code for the board (800013 for revision 000F) to work with rpi-lgpio module
+# os.environ['RPI_LGPIO_REVISION'] = '800013'
 
 import time
 import math
@@ -22,10 +22,10 @@ from LSM6DS3 import LSM6DS3
 bus = smbus2.SMBus(1)
 
 # Pin definitions
-pushButton = 11 # P1 header pin 11
+pushButton = 17 # BCM GPIO 17 = Pin 11 in P1 Header
 
-# Set pin-numbering scheme to P1 Board Header 
-GPIO.setmode(GPIO.BOARD)
+# Set pin-numbering scheme to BCM Pinout 
+GPIO.setmode(GPIO.BCM)
 
 # Set GPIO pins to I/O and activate internal pull-up resistances
 GPIO.setup(pushButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -190,73 +190,73 @@ if __name__ == "__main__":
 
     try:
         while True:
-            match current_menu:
-                case option.MAIN_MENU:
-                    print("Press to start measurement...")
+            if current_menu == option.MAIN_MENU:
+                
+                print("Press to start measurement...")
 
-                    while current_menu == option.MAIN_MENU:
-                        time.sleep(0.1)
+                while current_menu == option.MAIN_MENU:
+                    time.sleep(0.1)
 
-                case option.MEASURING_MODE:
-                    print("Measuring Mode")
+            elif current_menu == option.MEASURING_MODE:
+                print("Measuring Mode")
 
-                    if measurement is measure.START:
-                        if measure_mode is measure.DISTANCE:
-                            print("Measuring distance...")  
-                            print("\033[2A", end="")  # Move cursor up 2 lines
-                            acc_y = lsm6ds3.read_acceleration_y() * acc_scaling_factor
-                            vel_y += acc_y * sampling_interval
-                            dist_y += vel_y * sampling_interval  
-                            time.sleep(sampling_interval) 
-                        else:
-                            print("Measuring rotation...")
-                            print("\033[2A", end="")  # Move cursor up 2 lines
-                            dps_y = lsm6ds3.read_gyroscope_y() * acc_scaling_factor
-                            deg_y += dps_y * sampling_interval
-                            time.sleep(sampling_interval)  
-                    elif measurement is measure.STOP:
-                        measurement = measure.IDLE #In between measurements
-                        if measure_mode is measure.DISTANCE:
-                            distances.append(dist_y) # Saving last measured distance
-                            measure_mode = measure.ANGLE
-                        elif measure_mode is measure.ANGLE:
-                            rotations.append(deg_y) # Saving last measured roatation
-                            measure_mode = measure.DISTANCE
+                if measurement is measure.START:
+                    if measure_mode is measure.DISTANCE:
+                        print("Measuring distance...")  
+                        print("\033[2A", end="")  # Move cursor up 2 lines
+                        acc_y = lsm6ds3.read_acceleration_y() * acc_scaling_factor
+                        vel_y += acc_y * sampling_interval
+                        dist_y += vel_y * sampling_interval  
+                        time.sleep(sampling_interval) 
                     else:
-                        while current_menu is option.MEASURING_MODE and measurement is measure.IDLE:
-                            time.sleep(0.1)
-
-                case option.DIAG_MODE:
-                    print("Diagnostic Mode")                    
-                    
-                    acc_x = lsm6ds3.read_acceleration_x() * acc_scaling_factor
-                    acc_y = lsm6ds3.read_acceleration_y() * acc_scaling_factor
-                    acc_z = lsm6ds3.read_acceleration_z() * acc_scaling_factor
-
-                    dps_x = lsm6ds3.read_gyroscope_x() * acc_scaling_factor
-                    dps_y = lsm6ds3.read_gyroscope_y() * acc_scaling_factor
-                    dps_z = lsm6ds3.read_gyroscope_z() * acc_scaling_factor
-
-                    print(f"Accel (g): X={acc_x:.3f}, Y={acc_y:.3f}, Z={acc_z:.3f}")
-                    print(f"Ang Rate (dps): X={dps_x:.3f}, Y={dps_y:.3f}, Z={dps_z:.3f}")
-                    print("\033[2A", end="")  # Move cursor up 2 lines
-                    
-                    while current_menu == option.DIAG_MODE:
+                        print("Measuring rotation...")
+                        print("\033[2A", end="")  # Move cursor up 2 lines
+                        dps_y = lsm6ds3.read_gyroscope_y() * acc_scaling_factor
+                        deg_y += dps_y * sampling_interval
+                        time.sleep(sampling_interval)  
+                elif measurement is measure.STOP:
+                    measurement = measure.IDLE #In between measurements
+                    if measure_mode is measure.DISTANCE:
+                        distances.append(dist_y) # Saving last measured distance
+                        measure_mode = measure.ANGLE
+                    elif measure_mode is measure.ANGLE:
+                        rotations.append(deg_y) # Saving last measured roatation
+                        measure_mode = measure.DISTANCE
+                else:
+                    while current_menu is option.MEASURING_MODE and measurement is measure.IDLE:
                         time.sleep(0.1)
 
-                case option.SETTINGS_MENU:
-                    print("Settings")         
+            elif current_menu == option.DIAG_MODE:
+                print("Diagnostic Mode")                    
+                    
+                acc_x = lsm6ds3.read_acceleration_x() * acc_scaling_factor
+                acc_y = lsm6ds3.read_acceleration_y() * acc_scaling_factor
+                acc_z = lsm6ds3.read_acceleration_z() * acc_scaling_factor
 
-                    #TODO: Create inital menu to select resulution and sampling time
-                    acc_scaling_factor = 0.000061 # Sensitivity/Resolution for +-2g scale
-                    dps_scaling_factor = 0.0035  # Sensitivity/Resolution for +-1000dps scale
+                dps_x = lsm6ds3.read_gyroscope_x() * acc_scaling_factor
+                dps_y = lsm6ds3.read_gyroscope_y() * acc_scaling_factor
+                dps_z = lsm6ds3.read_gyroscope_z() * acc_scaling_factor
 
-                    while current_menu == option.SETTINGS_MENU:
-                        time.sleep(0.1)
+                print(f"Accel (g): X={acc_x:.3f}, Y={acc_y:.3f}, Z={acc_z:.3f}")
+                print(f"Ang Rate (dps): X={dps_x:.3f}, Y={dps_y:.3f}, Z={dps_z:.3f}")
+                print("\033[2A", end="")  # Move cursor up 2 lines
+                    
+                while current_menu == option.DIAG_MODE:
+                    time.sleep(0.1)
 
-                case _:
-                    current_menu = option.MAIN_MENU
-                    print("Default case")
+            elif current_menu == option.SETTINGS_MENU:
+                print("Settings")         
+
+                #TODO: Create inital menu to select resulution and sampling time
+                acc_scaling_factor = 0.000061 # Sensitivity/Resolution for +-2g scale
+                dps_scaling_factor = 0.0035  # Sensitivity/Resolution for +-1000dps scale
+
+                while current_menu == option.SETTINGS_MENU:
+                    time.sleep(0.1)
+
+            else:
+                current_menu = option.MAIN_MENU
+                print("Default case")
 
     except IOError as e:
         print("Unable to communicate with LSM6DS3. Check connections.")
